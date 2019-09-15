cf3ed93b056e2501b7a5055f18f5d11c52034482
will31415 <william.ottawa@yahoo.com>
2019-09-15 03:54:33 -0400

    pray this works

---
+++ api.py
@@ -0,0 +1,8 @@
+# from flask_api import FlaskAPI
+from "programs/food/detect.py" import analyse
+
+# app = FlaskAPI(__name__)
+# 
+# @app.route('/analyse/',methods=['POST'])
+# def analyse():
+#     return {'request data': request.data}
